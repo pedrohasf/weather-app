67d25fd48f4d3c8ddc50613be807570e6e55bb51
pedrohasf <pedrohasftb@gmail.com>
2020-02-26 16:44:20 -0300

    Updates

---
+++ precache-manifest.cc5a5f7e19d074bb635bac04107e2573.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b093a81cb73a2995481ccdadd224b963",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "dab60b6c931965ab13ff",
+    "url": "/weather-app/static/css/main.3c9b3d3b.chunk.css"
+  },
+  {
+    "revision": "4605d6bc2daf66f28048",
+    "url": "/weather-app/static/js/2.47047c85.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/weather-app/static/js/2.47047c85.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dab60b6c931965ab13ff",
+    "url": "/weather-app/static/js/main.e5fda5d3.chunk.js"
+  },
+  {
+    "revision": "f95d929b2dc853b2601a",
+    "url": "/weather-app/static/js/runtime-main.0c118c13.js"
+  }
+]);
